b8efa8c44ca34ac5a0a5f6c0d4f572c730395a61
Richard White <w@rwhite.no>
2019-03-20 08:58:26 +0200

    Update data.sh

--- travis/data.sh
+++ travis/data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript -e "devtools::load_all('$PWD'); fhidata:::gen_data(file.path('$PWD','inst','createddata'))"
+Rscript -e "devtools::load_all('$PWD'); print('$PWD'); fhidata:::gen_data(file.path('$PWD','inst','createddata'))"
