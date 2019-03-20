7f7dc6fb0dd195b6eaed49cec01ca6fe49958226
Richard White <w@rwhite.no>
2019-03-20 12:36:07 +0200

    units

--- R/gen_map.R
+++ R/gen_map.R
@@ -1,10 +1,5 @@
 #' norway_map_counties
 norway_map_counties <- function() {
-  if (!requireNamespace("GADMTools", quietly = TRUE)) {
-    stop("Package \"GADMTools\" needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
-
   id <- NULL
   ind <- NULL
 
