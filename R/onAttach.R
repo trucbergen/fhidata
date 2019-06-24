99a5827b4a740fe3815a1db0df8eab545b96f730
Travis CI User <travis@example.org>
2019-06-24 16:14:10 +0000

    Travis CI update 28 [ci skip]

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: fhidata")
-  packageStartupMessage("Version: 2019.06.24 at 13:54")
+  packageStartupMessage("Version: 2019.06.24 at 15:47")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
