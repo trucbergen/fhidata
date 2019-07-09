47ba63e8fe76c6811081eae369c854fd94fc5350
Travis CI User <travis@example.org>
2019-07-09 10:07:23 +0000

    Travis CI update 29 [ci skip]

--- R/days.r
+++ R/days.r
@@ -29,13 +29,13 @@ gen_days <- function() {
   days <- data.table(day = seq.Date(as.IDate("2000-01-01"), as.IDate("2030-01-01"), by = "days"))
   days[, wkyr := format.Date(day, format = "%G-%V")]
   days <- days[, .(mon = as.IDate(min(day))), by = .(wkyr)]
-  days[,tue:=mon+1]
-  days[,wed:=mon+2]
-  days[,thu:=mon+3]
-  days[,fri:=mon+4]
-  days[,sat:=mon+5]
-  days[,sun:=mon+6]
-  days <- days[wkyr>="2000-01"]
+  days[, tue := mon + 1]
+  days[, wed := mon + 2]
+  days[, thu := mon + 3]
+  days[, fri := mon + 4]
+  days[, sat := mon + 5]
+  days[, sun := mon + 6]
+  days <- days[wkyr >= "2000-01"]
 
   return(days)
 }
--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: fhidata")
-  packageStartupMessage("Version: 2019.06.24 at 15:47")
+  packageStartupMessage("Version: 2019.07.09 at 09:42")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
