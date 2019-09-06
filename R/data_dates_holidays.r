db307e1c1a347618329ac95c00110e6180d5538a
Travis CI User <travis@example.org>
2019-09-06 13:39:01 +0000

    Travis CI update 41 [ci skip]

--- R/data_dates_holidays.r
+++ R/data_dates_holidays.r
@@ -27,9 +27,9 @@ gen_norway_dates_holidays <- function() {
   date_min <- as.Date(glue::glue("{year_min}-01-01"))
   date_max <- as.Date(glue::glue("{year_max}-12-31"))
 
-  norway_dates_holidays <- data.table(date=seq.Date(date_min, date_max, by=1))
-  norway_dates_holidays[,is_holiday:=FALSE]
-  norway_dates_holidays[info, on="date", is_holiday:=TRUE]
+  norway_dates_holidays <- data.table(date = seq.Date(date_min, date_max, by = 1))
+  norway_dates_holidays[, is_holiday := FALSE]
+  norway_dates_holidays[info, on = "date", is_holiday := TRUE]
 
   return(norway_dates_holidays)
 }
--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: fhidata")
-  packageStartupMessage("Version: 2019.08.29 at 06:48")
+  packageStartupMessage("Version: 2019.09.06 at 13:11")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
